75380da2b31eb8ff4f42e68dd968195b8fd75f0d
amanfred <alesmanfreda82@gmail.com>
2019-03-24 19:51:29 +0100

    Initial setup

---
+++ Clm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clm.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
